9ac2872938173c911f00cd9575874e6f76e5168c
Matheus Mello <mfmello@outlook.com>
2017-10-27 21:48:28 -0200

    Formulario e status bar ok

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -10,7 +10,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-      ionic.Platform.fullScreen(true, true);
+    ionic.Platform.fullScreen(true, true);
+
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
       cordova.plugins.Keyboard.disableScroll(true);
@@ -18,7 +19,10 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+      StatusBar.overlaysWebView(false);
+      StatusBar.backgroundColorByHexString('021f54');
+      StatusBar.styleLightContent();
+
     }
   });
 })
